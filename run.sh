3e163f8c75565233ccac536f8d749236707e37bc
Clemens Horn <clemens.horn@mni.thm.de>
2024-10-29 10:51:44 +0100

    feat(docker): pass stdin from host to container

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ CONTAINER="cryptology"
 CLI_NAME="$CONTAINER"
 CLI_ARGS="$@"
 
-docker run --rm -v "$(pwd)":/app/ $CONTAINER $CLI_NAME $CLI_ARGS
+docker run -i --rm -v "$(pwd)":/app/ $CONTAINER $CLI_NAME $CLI_ARGS
