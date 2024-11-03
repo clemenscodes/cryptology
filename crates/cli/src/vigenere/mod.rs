bac401dc75f676490a0035c2cd35b1b295e09121
Clemens Horn <clemens.horn@mni.thm.de>
2024-11-03 16:42:40 +0100

    test(vigenere): some boilerplate for testing

--- crates/cli/src/vigenere/mod.rs
+++ crates/cli/src/vigenere/mod.rs
@@ -1,6 +1,8 @@
-use std::io::{Read, Result, Write};
+use std::io::{Cursor, Read, Result, Write};
 
-use crate::Command;
+use crate::{
+  caesar::CaesarCipher, frequency_analysis::FrequencyAnalyzer, Command,
+};
 
 pub struct VigenereConfig {
   max_key_length: u8,
@@ -40,14 +42,87 @@ impl VigenereCypher {
     config: VigenereConfig,
   ) -> Result<()> {
     let mut content = String::new();
+
     input.read_to_string(&mut content)?;
 
-    for i in 1..=config.max_key_length {
-      println!("Checking keylength {i}");
+    let mut best_decryption = String::new();
+    let mut best_score = f32::MAX;
+
+    for key_length in 1..=config.max_key_length {
+      let mut columns: Vec<Vec<char>> = vec![Vec::new(); key_length as usize];
+
+      for (i, c) in content.chars().enumerate() {
+        columns[i % key_length as usize].push(c);
+      }
+
+      let mut key = String::new();
+
+      for column in columns.iter() {
+        let input: String = column.iter().collect();
+        let mut buf = Cursor::new(input.into_bytes());
+
+        let best_shift = Self::find_best_shift_for_column(&mut buf)?;
+        key.push((b'A' + best_shift) as char);
+      }
+
+      let decipher = Self::decrypt_vigenere(&content, &key);
+      let mut input = Cursor::new(decipher.clone().into_bytes());
+      let mut output = Vec::new();
+      let fa = FrequencyAnalyzer::analyze(&mut input, &mut output)?;
+      let score = FrequencyAnalyzer::chi_square_score(&fa);
+
+      if score < best_score {
+        best_score = score;
+        best_decryption = decipher;
+      }
     }
 
-    write!(output, "{content}")?;
+    write!(output, "{best_decryption}")?;
     Ok(())
+  }
+
+  fn find_best_shift_for_column<R: Read>(column: &mut R) -> Result<u8> {
+    let mut best_shift = 0;
+    let mut best_score = f32::MAX;
+    let mut buf = String::new();
+
+    column.read_to_string(&mut buf)?;
+
+    for shift in 0..26 {
+      let copy = buf.clone();
+      let mut cursor = Cursor::new(copy.as_bytes());
+      let decipher = CaesarCipher::decrypt_caesar_cipher(&mut cursor, shift)?;
+      let mut input = Cursor::new(decipher.into_bytes());
+      let mut output = Vec::new();
+      let fa = FrequencyAnalyzer::analyze(&mut input, &mut output)?;
+      let score = FrequencyAnalyzer::chi_square_score(&fa);
+
+      if score < best_score {
+        best_score = score;
+        best_shift = shift;
+      }
+    }
+
+    Ok(best_shift)
+  }
+
+  fn decrypt_vigenere(content: &str, key: &str) -> String {
+    content
+      .chars()
+      .enumerate()
+      .map(|(i, c)| {
+        if c.is_ascii_alphabetic() {
+          let key_char = key.as_bytes()[i % key.len()];
+          let offset = if c.is_ascii_uppercase() { b'A' } else { b'a' };
+          let c_value = c as u8 - offset;
+          let key_value = key_char - b'A';
+          let shifted = (26 + c_value - key_value) % 26;
+          (shifted + offset) as char
+        } else {
+          c
+        }
+      })
+      .collect()
   }
 }
 
