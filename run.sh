32bc342f64ecaf811b510045812074336f750e58
Clemens Horn <clemens.horn@mni.thm.de>
2024-11-05 22:02:01 +0100

    chore(build): update input

--- run.sh
+++ run.sh
@@ -2,6 +2,11 @@
 
 CONTAINER="cryptology"
 CLI_NAME="$CONTAINER"
-CLI_ARGS="$@"
+
+if [ -z "$1" ]; then
+  CLI_ARGS="decrypt vigenere --input ciphertext.txt --output solution.txt"
+else
+  CLI_ARGS="$@"
+fi
 
 docker run -i --rm -v "$(pwd)":/app/ $CONTAINER $CLI_NAME $CLI_ARGS
